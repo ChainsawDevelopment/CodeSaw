2080488fac6d51018fe0f61960f4b6a8ac364239
Maciej Nowak <maciejt.nowak@gmail.com>
2018-05-22 20:07:43 +0200

    Separated 4-way diff

---
+++ Tests/DiffClassifier.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using DiffMatchPatch;
+
+namespace Tests
+{
+    public class DiffClassifier
+    {
+        public static List<ClassifiedDiff> ClassifyDiffs(IEnumerable<Diff> baseDiff, IEnumerable<Diff> reviewDiff)
+        {
+            var classified = new List<ClassifiedDiff>();
+
+            var chunksFromBaseDiff = new List<Diff>(baseDiff);
+
+            foreach (var reviewChunk in reviewDiff)
+            {
+                if (Equals(reviewChunk.Operation, Operation.Equal))
+                {
+                    classified.Add(new ClassifiedDiff(reviewChunk, DiffClassification.Unchanged));
+                    continue;
+                }
+
+                var matchingBaseChunkIdx = chunksFromBaseDiff.IndexOf(reviewChunk, new DiffEqualityComparer());
+
+                if (matchingBaseChunkIdx >= 0)
+                {
+                    chunksFromBaseDiff.RemoveRange(0, matchingBaseChunkIdx + 1);
+                    classified.Add(new ClassifiedDiff(reviewChunk, DiffClassification.BaseChange));
+                    continue;
+                }
+
+                classified.Add(new ClassifiedDiff(reviewChunk, DiffClassification.ReviewChange));
+            }
+
+            return classified;
+        }
+    }
+
+    public enum DiffClassification
+    {
+        Unchanged,
+        BaseChange,
+        ReviewChange
+    }
+
+    public class ClassifiedDiff
+    {
+        public Diff Diff { get; }
+        public DiffClassification Classification { get; }
+
+        public ClassifiedDiff(Diff diff, DiffClassification classification)
+        {
+            Diff = diff;
+            Classification = classification;
+        }
+
+        public override string ToString() => $"{Classification}({Diff})";
+    }
+}
