6cedc0a3211e40f3b6739de657776298332d3234
Maciej Nowak <maciejt.nowak@gmail.com>
2018-06-07 21:25:11 +0200

    Popup with file list

---
+++ Web/frontend/pages/review/fileTreePopup.tsx
@@ -0,0 +1,64 @@
+import Popup from 'semantic-ui-react/dist/commonjs/modules/Popup';
+import Button from 'semantic-ui-react/dist/commonjs/elements/Button';
+
+import * as React from "react";
+import { RevisionRangeInfo, PathPair, emptyPathPair } from "../../api/reviewer";
+import { FileInfo } from "./state";
+import ChangedFileTree from './changedFileTree';
+
+interface State {
+    opened: boolean;
+}
+
+type SelectFileForViewHandler = (path: PathPair) => void;
+
+export interface Props {
+    paths: PathPair[];
+    selected:PathPair;
+    onSelect(path: PathPair): void;
+}
+
+export default class ChangedFileTreePopup extends React.Component<Props, State> {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            opened: false
+        };
+    }
+
+    private _onOpen = () => {
+        this.setState({ opened: true });
+    }
+
+    private _onClose = () => {
+        this.setState({ opened: false });
+    }
+
+    private _onSelect = (p: PathPair) => {
+        this.props.onSelect(p);
+        this.setState({opened: false});
+    }
+
+    render() {
+        const filesSelector = (
+            <ChangedFileTree
+                paths={this.props.paths}
+                selected={this.props.selected}
+                onSelect={this._onSelect}
+            />
+        );
+
+        return (
+            <Popup
+                open={this.state.opened}
+                trigger={<Button color='green' content='Files' />}
+                content={filesSelector}
+                onOpen={this._onOpen}
+                onClose={this._onClose}
+                on='click'
+                position='top right'
+            />
+        );
+    }
+}
--- Web/frontend/pages/review/index.tsx
+++ Web/frontend/pages/review/index.tsx
@@ -15,6 +15,7 @@ import Popup from 'semantic-ui-react/dist/commonjs/modules/Popup';
 import VersionSelector from './versionSelector';
 import ChangedFileTree from './changedFileTree';
 import DiffView from './diffView';
+import ChangedFileTreePopup from './fileTreePopup'
 
 import "./review.less";
 import { OnMount } from "../../components/OnMount";
@@ -45,23 +46,14 @@ const FileSummary = (props: { file: FileInfo }): JSX.Element => {
     );
 };
 
-const RangeInfo = (props: { info: RevisionRangeInfo, selectedFile: FileInfo, onSelectFileForView: SelectFileForViewHandler }): JSX.Element => {
-    const filesSelector = (
-        <ChangedFileTree
-            paths={props.info.changes.map(i => i.path)}
-            selected={props.selectedFile ? props.selectedFile.path : emptyPathPair}
-            onSelect={props.onSelectFileForView}
-        />
-    );
-
+const RangeInfo = (props: { info: RevisionRangeInfo, selectedFile: FileInfo, onSelectFileForView: SelectFileForViewHandler }): JSX.Element => {   
     return (
         <div style={{ flex: 1 }}>
             <div>
-                <Popup
-                    trigger={<Button color='green' content='Files' />}
-                    content={filesSelector}
-                    on='click'
-                    position='top right'
+                <ChangedFileTreePopup
+                    paths={props.info.changes.map(i => i.path)}
+                    selected={props.selectedFile ? props.selectedFile.path : emptyPathPair}
+                    onSelect={props.onSelectFileForView}
                 />
             </div>
             <Segment basic>
@@ -71,23 +63,6 @@ const RangeInfo = (props: { info: RevisionRangeInfo, selectedFile: FileInfo, onS
             </Segment>
         </div>
     )
-
-    // return (
-    //     <div style={{ flex: 1 }}>
-    //         <Sidebar.Pushable as={Segment}>
-    //             <Sidebar visible={true} width='thin'>
-    //                 <ChangedFileTree
-    //                     paths={props.info.changes.map(i => i.path)}
-    //                     selected={props.selectedFile ? props.selectedFile.path : emptyPathPair}
-    //                     onSelect={props.onSelectFileForView}
-    //                 />
-    //             </Sidebar>
-    //             <Sidebar.Pusher>
-                    
-    //             </Sidebar.Pusher>
-    //         </Sidebar.Pushable>
-    //     </div>
-    // );
 }
 
 interface OwnProps {
