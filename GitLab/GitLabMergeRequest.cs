cce181453a4767206727c8cdfcd3ce03b4c1d575
Marek Antoniak <kfazol@gmail.com>
2018-07-21 13:00:10 +0200

    Restored missing review user names

---
+++ GitLab/GitLabMergeRequest.cs
@@ -0,0 +1,18 @@
+﻿using RepositoryApi;
+
+namespace GitLab
+{
+    public class GitLabMergeRequest
+    {
+        public int Id { get; set; }
+        public int ProjectId { get; set; }
+        public string Title { get; set; }
+        public GitLabUserInfo Author { get; set; }
+        public string BaseCommit { get; set; }
+        public string HeadCommit { get; set; }
+        public string Description { get; set; }
+        public MergeRequestState State { get; set; }
+
+        public MergeStatus MergeStatus { get; set; }
+    }
+}
