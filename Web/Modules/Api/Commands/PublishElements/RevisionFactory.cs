cdec0c0ce57abce61a3e536f2095ac0e4ce01a50
Maciej Nowak <maciejt.nowak@gmail.com>
2018-07-30 21:12:45 +0200

    Building FileMatrix

---
+++ Web/Modules/Api/Commands/PublishElements/RevisionFactory.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NHibernate;
+using RepositoryApi;
+using Web.Modules.Api.Model;
+
+namespace Web.Modules.Api.Commands.PublishElements
+{
+    public class RevisionFactory
+    {
+        private readonly IRepository _api;
+        private readonly ISession _session;
+
+        public RevisionFactory(IRepository api, ISession session)
+        {
+            _api = api;
+            _session = session;
+        }
+
+        public async Task<ReviewRevision> Create(ReviewIdentifier reviewId, int number, string baseCommit, string headCommit)
+        {
+            var revision = new ReviewRevision
+            {
+                Id = GuidComb.Generate(),
+                ReviewId = reviewId,
+                RevisionNumber = number,
+                BaseCommit = baseCommit,
+                HeadCommit = headCommit
+            };
+
+            string previousHead;
+
+            if (number > 1)
+            {
+                var previousRevision = FindPreviousRevision(reviewId, number);
+                previousHead = previousRevision.HeadCommit;
+            }
+            else
+            {
+                previousHead = baseCommit;
+            }
+
+            var diff = await _api.GetDiff(reviewId.ProjectId, previousHead, headCommit);
+
+            foreach (var file in diff)
+            {
+                revision.Files.Add(RevisionFile.FromDiff(file));
+            }
+
+            return revision;
+        }
+
+        private ReviewRevision FindPreviousRevision(ReviewIdentifier reviewId, int number)
+        {
+            return _session.Query<ReviewRevision>().Single(x => x.ReviewId == reviewId && x.RevisionNumber == number - 1);
+        }
+    }
+}
--- Web/Modules/Api/Model/ReviewRevision.cs
+++ Web/Modules/Api/Model/ReviewRevision.cs
@@ -17,7 +17,31 @@ namespace Web.Modules.Api.Model
         public virtual string HeadCommit { get; set; }
         public virtual string BaseCommit { get; set; }
 
+        public virtual IList<RevisionFile> Files { get; set; } = new List<RevisionFile>();
+
         public virtual DateTimeOffset LastUpdatedAt { get; set; }
+    }
+
+    public class RevisionFile
+    {
+        public virtual Guid Id { get; set; }
+        public virtual PathPair File { get; set; }
+        public virtual bool IsNew { get; set; }
+        public virtual bool IsDeleted { get; set; }
+        public virtual bool IsRenamed { get; set; }
+        public virtual DateTimeOffset LastUpdatedAt { get; set; }
+
+        public static RevisionFile FromDiff(FileDiff file)
+        {
+            return new RevisionFile
+            {
+                Id = GuidComb.Generate(),
+                File = file.Path,
+                IsNew = file.NewFile,
+                IsDeleted = file.DeletedFile,
+                IsRenamed = file.RenamedFile
+            };
+        }
     }
 
     public class ReviewRevisionMapping : ClassMapping<ReviewRevision>
@@ -36,6 +60,22 @@ namespace Web.Modules.Api.Model
             Property(x => x.RevisionNumber);
             Property(x => x.HeadCommit, p => p.Length(40));
             Property(x => x.BaseCommit, p => p.Length(40));
+
+            Bag(x => x.Files, coll =>
+            {
+                coll.Key(key =>
+                {
+                    key.Column(c=>
+                    {
+                        c.Name("RevisionId");
+                        c.NotNullable(true);
+                    });
+                });
+                coll.Cascade(Cascade.All);
+            }, map =>
+            {
+                map.OneToMany();
+            });
         }
     }
 
@@ -45,6 +85,26 @@ namespace Web.Modules.Api.Model
         {
             Property(x => x.ProjectId);
             Property(x => x.ReviewId);
+        }
+    }
+
+    public class RevisionFileMapping : ClassMapping<RevisionFile>
+    {
+        public RevisionFileMapping()
+        {
+            Table("RevisionFiles");
+
+            Id(x => x.Id, id => id.Generator(Generators.Assigned));
+            Version(x => x.LastUpdatedAt, v =>
+            {
+                v.Type(new DateTimeOffsetType());
+            });
+
+            Component(x => x.File);
+
+            Property(x => x.IsNew);
+            Property(x => x.IsDeleted);
+            Property(x => x.IsRenamed);
         }
     }
 }
