49520adff221e6baad6011d5b5f0391e96b439a9
Maciej Nowak <maciejt.nowak@gmail.com>
2018-09-04 17:28:08 +0200

    Removed comments

--- Web/Modules/Api/Model/FileDiscussionConfig.cs
+++ Web/Modules/Api/Model/FileDiscussionConfig.cs
@@ -9,34 +9,8 @@ namespace Web.Modules.Api.Model
         public FileDiscussionConfig()
         {
             Table("FileDiscussions");
-            //Id(x => x.Id, id => id.Generator(Generators.Assigned));
-            //Version(x => x.LastUpdatedAt, v => { v.Type(new DateTimeOffsetType()); });
-
-            //Property(x => x.RevisionId);
-
             Component(x => x.File);
             Property(x => x.LineNumber);
-
-            //ManyToOne(x => x.RootComment, mto =>
-            //{
-            //    mto.Unique(true);
-
-            //    mto.NotNullable(true);
-            //    mto.Column(c=>
-            //    {
-            //        c.NotNullable(true);
-            //        c.Name("RootCommentId");
-            //    });
-
-            //    mto.Class(typeof(Comment));
-            //    mto.Cascade(Cascade.All.Include(Cascade.DeleteOrphans));
-            //});
-
-            //Property(x => x.State, mapper =>
-            //{
-            //    mapper.Type<EnumStringType<CommentState>>();
-            //    mapper.NotNullable(true);
-            //});
         }
     }
 
--- Web/Modules/Api/Model/ReviewDiscussionConfig.cs
+++ Web/Modules/Api/Model/ReviewDiscussionConfig.cs
@@ -9,31 +9,6 @@ namespace Web.Modules.Api.Model
         public ReviewDiscussionConfig()
         {
             Table("ReviewDiscussions");
-            //Id(x => x.Id, id => id.Generator(Generators.Assigned));
-            //Version(x => x.LastUpdatedAt, v => { v.Type(new DateTimeOffsetType()); });
-
-            //Property(x => x.RevisionId);
-
-            //ManyToOne(x => x.RootComment, mto =>
-            //{
-            //    mto.Unique(true);
-
-            //    mto.NotNullable(true);
-            //    mto.Column(c=>
-            //    {
-            //        c.NotNullable(true);
-            //        c.Name("RootCommentId");
-            //    });
-
-            //    mto.Class(typeof(Comment));
-            //    mto.Cascade(Cascade.All.Include(Cascade.DeleteOrphans));
-            //});
-
-            //Property(x => x.State, mapper =>
-            //{
-            //    mapper.Type<EnumStringType<CommentState>>();
-            //    mapper.NotNullable(true);
-            //});
         }
     }
 }
