5e4df65bf3b2d775712feb326daa8e58163462e9
Krystian Przybyła - Warmuziński <kprzybyla@kplabs.pl>
2018-10-22 19:12:02 +0200

    Display author at review info - Migration fixed

--- CodeSaw.Db.Migrations/2018_10_05_18_46_UserGivenNameAlteredToName.cs
+++ CodeSaw.Db.Migrations/2018_10_05_18_46_UserGivenNameAlteredToName.cs
@@ -7,24 +7,12 @@ namespace Db.Migrations
     {
         public override void Up()
         {
-            Alter.Table("Users")
-                .InSchema("dbo")
-                .AddColumn("Name").AsString(200).Nullable();
-
-            Execute.Sql("UPDATE dbo.Users SET Name = (SELECT GivenName from dbo.Users c where c.Id = Id)");
-
-            Delete.Column("GivenName").FromTable("Users").InSchema("dbo");
+            Rename.Column("GivenName").OnTable("Users").To("Name");
         }
 
         public override void Down()
         {
-            Alter.Table("Users")
-                .InSchema("dbo")
-                .AddColumn("GivenName").AsString(200).Nullable();
-
-            Execute.Sql("UPDATE c SET c.GivenName = d.Name FROM dbo.Users c JOIN dbo.Users d on d.Id = c.Id");
-
-            Delete.Column("Name").FromTable("Users").InSchema("dbo");
+            Rename.Column("Name").OnTable("Users").To("GivenName");
         }
     }
 }
