275f0eb4a4ece3c6b4fc3267bfcb7d9d451f3758
Maciej Nowak <maciejt.nowak@gmail.com>
2018-10-24 19:14:12 +0200

    Remove comment length limit

---
+++ CodeSaw.Db.Migrations/2018_10_24_19_00_RemoveCommentLengthLimit.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace CodeSaw.Db.Migrations
+{
+    [Migration(2018_10_24_19_00)]
+    public class Migration_2018_10_24_19_00_RemoveCommentLengthLimit: Migration
+
+    {
+        public override void Up()
+        {
+            Alter.Column("Content").OnTable("Comments").InSchema("dbo").AsMaxString();
+        }
+
+        public override void Down()
+        {
+            Alter.Column("Content").OnTable("Comments").InSchema("dbo").AsString(255);
+        }
+    }
+}
---
+++ CodeSaw.Db.Migrations/MigrationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator.Builders.Alter.Column;
+using FluentMigrator.Builders.Create.Table;
+
+namespace CodeSaw.Db.Migrations
+{
+    internal static class MigrationExtensions
+    {
+        public static ICreateTableColumnOptionOrWithColumnSyntax AsMaxString(this ICreateTableColumnAsTypeSyntax column)
+        {
+            return column.AsString(int.MaxValue);
+        }
+
+        public static IAlterColumnOptionSyntax AsMaxString(this IAlterColumnAsTypeSyntax column)
+        {
+            return column.AsString(int.MaxValue);
+        }
+    }
+}
