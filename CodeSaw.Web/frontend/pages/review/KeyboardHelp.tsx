4c52ae1559a37df187a58c99b28f700fcb0ea60e
Marcin Drobik <mdrobik@kplabs.pl>
2018-10-29 17:17:52 +0100

    Keyboard help popup

---
+++ CodeSaw.Web/frontend/pages/review/KeyboardHelp.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react'
+import Modal from 'semantic-ui-react/dist/commonjs/modules/Modal'
+import "./keyboardHelp.less"
+
+interface Props {
+    isOpen: boolean
+    handleClose(): void;
+}
+
+export class KeyboardHelp extends React.Component<Props> {
+
+    render(): JSX.Element {
+        return <div className={"keyboard-help"}>
+            <Modal
+                open={this.props.isOpen}
+                onClose={this.props.handleClose}
+                className={"keyboard-help"}
+            >
+                <Modal.Header>Keyboard Shortcuts</Modal.Header>
+                <Modal.Content>
+                    <Modal.Description>
+                        <ul>
+                            <li><code>?</code> - this help</li>
+                            <li><code>[ ]</code> - previous/next unreviewed file</li>
+                            <li><code>ctrl+p</code> - go to file by name</li>
+                            <li><code>y</code> - mark file as reviewed/unreviewed</li>
+                            <li><code>ctrl+enter</code> - publish current review</li>
+                        </ul>
+                    </Modal.Description>
+                </Modal.Content>
+            </Modal>
+        </div >
+    }
+}
