c397a9bf8c8ab74f47cf3636a6142a0202786af8
Maciej Nowak <maciejt.nowak@gmail.com>
2018-10-30 18:23:59 +0100

    Generate FileHistory table

--- CodeSaw.Db.Migrations/MigrationExtensions.cs
+++ CodeSaw.Db.Migrations/MigrationExtensions.cs
@@ -1,4 +1,7 @@
-﻿using FluentMigrator.Builders.Alter.Column;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using FluentMigrator.Builders.Alter.Column;
 using FluentMigrator.Builders.Create.Table;
 
 namespace CodeSaw.Db.Migrations
@@ -13,6 +16,55 @@ namespace CodeSaw.Db.Migrations
         public static IAlterColumnOptionSyntax AsMaxString(this IAlterColumnAsTypeSyntax column)
         {
             return column.AsString(int.MaxValue);
+        }
+
+        public static IDataReader ExecuteQuery(this IDbTransaction transaction, string sql)
+        {
+            var command = transaction.Connection.CreateCommand();
+            command.Transaction = transaction;
+            command.CommandText = sql;
+            return command.ExecuteReader();
+        }
+
+        public static List<T> ToList<T>(this IDataReader reader, Func<IDataReader, T> projection)
+        {
+            var result = new List<T>();
+
+            while (reader.Read())
+            {
+                result.Add(projection(reader));
+            }
+
+            reader.Dispose();
+
+            return result;
+        }
+
+        public static IDbCommand CreateCommand(this IDbTransaction transaction, string sql)
+        {
+            var command = transaction.Connection.CreateCommand();
+            command.Transaction = transaction;
+
+            command.CommandText = sql;
+
+            return command;
+        }
+
+        public static void CreateParameter(this IDbCommand cmd, string name, DbType type)
+        {
+            var parameter = cmd.CreateParameter();
+            parameter.DbType = type;
+            parameter.ParameterName = name;
+            cmd.Parameters.Add(parameter);
+        }
+
+        public static void CreateParameter(this IDbCommand cmd, string name, DbType type, object value)
+        {
+            var parameter = cmd.CreateParameter();
+            parameter.DbType = type;
+            parameter.ParameterName = name;
+            parameter.Value = value ?? DBNull.Value;
+            cmd.Parameters.Add(parameter);
         }
     }
 }
