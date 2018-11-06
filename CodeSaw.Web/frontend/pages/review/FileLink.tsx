7849a2ee4162f608ad7da234a1024bfb592be252
Maciej Nowak <maciejt.nowak@gmail.com>
2018-11-06 10:42:38 +0100

    Show file names instead of ID

--- CodeSaw.Web/frontend/pages/review/FileLink.tsx
+++ CodeSaw.Web/frontend/pages/review/FileLink.tsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
 import { ReviewId, FileId } from "../../api/reviewer";
-import * as PathPairs from "../../pathPair";
+import FileName from './fileName';
 
 export const createLinkToFile = (reviewId: ReviewId, fileId: FileId) : string => (
     `/project/${reviewId.projectId}/review/${reviewId.reviewId}/${fileId}`);
 
-export const FileLink = (props: {reviewId: ReviewId, fileId: FileId, path: PathPairs.PathPair, children?: any}) => {
+export const FileLink = (props: {reviewId: ReviewId, fileId: FileId, children?: any}) => {
     return <Link 
         to={createLinkToFile(props.reviewId, props.fileId)}>
-        {props.children || props.path.newPath}
+        {props.children || <FileName fileId={props.fileId} />}
     </Link>
 }
---
+++ CodeSaw.Web/frontend/pages/review/FileName.tsx
@@ -0,0 +1,24 @@
+import { PathPair, make } from '../../pathPair';
+import { RootState } from '../../rootState';
+import { connect } from 'react-redux';
+import * as React from 'react';
+
+interface OwnProps {
+    fileId: string;
+}
+
+interface StateProps {
+    fileName: PathPair;
+}
+
+const FileName = (props: OwnProps & StateProps): JSX.Element => {
+    return <span>{props.fileName.newPath}</span>;
+}
+
+const mapStateToProps = (state: RootState, ownProps: OwnProps) => ({
+    fileName: state.review.currentReview.fileMatrix.find(f => f.fileId == ownProps.fileId).file
+});
+
+export default connect(
+    mapStateToProps
+)(FileName);
