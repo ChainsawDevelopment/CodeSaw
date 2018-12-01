c5a7b472dc46fa48dfe1f8ad0d5fe13b71e2ad4b
Maciej Nowak <maciejt.nowak@gmail.com>
2018-12-01 16:42:28 +0100

    Fake terget for creating new migrations

---
+++ utils/MigrationTemplate.cs
@@ -0,0 +1,18 @@
+using System.Data;
+using FluentMigrator;
+
+namespace CodeSaw.Db.Migrations
+{
+    [Migration([Id])]
+    public class Migration_[Id]_[Name] : Migration
+    {
+        public override void Up()
+        {
+        }
+
+        public override void Down()
+        {
+            
+        }
+    }
+}
