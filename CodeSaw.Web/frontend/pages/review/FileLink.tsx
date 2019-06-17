31b28d5041ce87472aef39b8de57e0b09d3ad79f
Maciej Nowak <maciejt.nowak@gmail.com>
2019-06-17 08:20:31 +0200

    Fixed unscrollable file popup

--- CodeSaw.Web/frontend/pages/review/FileLink.tsx
+++ CodeSaw.Web/frontend/pages/review/FileLink.tsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 import { ReviewId, FileId } from "../../api/reviewer";
 import FileName from './FileName';
 
-export const createLinkToFile = (reviewId: ReviewId, fileId: FileId) : string => (
+export const createLinkToFile = (reviewId: ReviewId, fileId: FileId): string => (
     `/project/${reviewId.projectId}/review/${reviewId.reviewId}/${fileId}`);
 
-export const FileLink = (props: {reviewId: ReviewId, fileId: FileId, children?: any}) => {
-    return <Link 
-        to={createLinkToFile(props.reviewId, props.fileId)}>
+export const FileLink = (props: { reviewId: ReviewId, fileId: FileId, children?: any, onClick?: () => void }) => {
+    return <Link
+        to={createLinkToFile(props.reviewId, props.fileId)} onClick={props.onClick}>
         {props.children || <FileName fileId={props.fileId} />}
     </Link>
 }
