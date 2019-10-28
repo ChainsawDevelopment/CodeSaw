f6a75d446a93ed00a94ab561e68d7ba2e4012072
Marek Antoniak <mantoniak@kplabs.pl>
2019-10-28 10:39:01 +0100

    Implemented WriteJson for ClientFileIdConverter

--- CodeSaw.Web/Modules/Api/Commands/ClientFileId.cs
+++ CodeSaw.Web/Modules/Api/Commands/ClientFileId.cs
@@ -32,7 +32,7 @@ namespace CodeSaw.Web.Modules.Api.Commands
         {
             public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
             {
-                throw new NotSupportedException();
+                writer.WriteValue(value);
             }
 
             public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
