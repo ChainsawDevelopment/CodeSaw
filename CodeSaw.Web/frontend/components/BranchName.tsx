390bb6acd88e1c68fee33341ad09ba5514338141
Marcin Drobik <mdrobik@kplabs.pl>
2020-03-26 18:32:58 +0100

    Possibility to copy branch name

--- CodeSaw.Web/frontend/components/BranchName.tsx
+++ CodeSaw.Web/frontend/components/BranchName.tsx
@@ -1,6 +1,18 @@
 import * as React from "react";
+import * as Clipboard from "clipboard"
+import Icon from "@ui/elements/Icon";
+import Popup from "@ui/modules/Popup";
+
 import "./branchName.less";
 
-const Branch = (props: { name:string }): JSX.Element => (<span className='branch-name'>{props.name}</span>);
+const Branch = (props: { name: string }): JSX.Element => (<span id={props.name} className='branch-name'>{props.name}
+    {props.name ? <Popup content='Copy branch name' trigger={
+        <Icon className={"copy-branch-name"} data-clipboard-text={props.name} color='black' name='copy' />
+    } />
+    : null}
+    
+</span>);
+
+new Clipboard('.copy-branch-name');
 
 export default Branch;
