bdef9691d1e8315061687739c4b56ff1c3bc9d07
Maciej Nowak <maciejt.nowak@gmail.com>
2020-03-29 12:40:24 +0200

    Split RangeInfo component into sections

--- CodeSaw.Web/frontend/pages/review/rangeInfo.tsx
+++ CodeSaw.Web/frontend/pages/review/rangeInfo.tsx
@@ -1,139 +1,19 @@
 import * as React from "react";
 
-import Menu from '@ui/collections/Menu';
-import Button from '@ui/elements/Button';
 import Segment from '@ui/elements/Segment';
 import Sticky from '@ui/modules/Sticky';
-import Icon from '@ui/elements/Icon';
-import Popup from '@ui/modules/Popup';
 import scrollToComponent from 'react-scroll-to-component';
-import { FileLink } from "./FileLink";
 
 import * as PathPairs from "../../pathPair";
-import { ReviewId, FileDiscussion, CommentReply, FileToReview, FileId } from "../../api/reviewer";
+import { FileDiscussion, FileToReview, FileId } from "../../api/reviewer";
 import { FileInfo } from "./state";
 
-import CommentedDiffView, { LineCommentsActions } from './commentedDiffView';
-import { DiscussionActions, getNewDiscussionTextAreaId } from "./commentsView";
-import FileSummary from './fileSummary';
-import ChangedFileTreePopup from "./fileTreePopup";
-import ReviewMark from "./reviewMark";
-import { UserState } from "../../rootState";
-import { DiffType } from "./diffView";
 import { HotKeys } from "../../components/HotKeys";
-import { PublishButton } from "./PublishButton";
-import ReviewMode from "./reviewMode";
 
-import vscodeLogo from '../../assets/vscode.png'
-
-interface FileViewProps {
-    file: FileInfo;
-    comments: FileDiscussion[];
-    unpublishedFileDiscussions: FileDiscussion[];
-    pendingResolved: string[];
-    unpublishedReplies: CommentReply[];
-    commentActions: DiscussionActions;
-    currentUser: UserState;
-    startFileDiscussion(fileId: FileId, lineNumber: number, content: string, needsResolution: boolean): void;
-    scrollToFile(): void;
-}
-
-class FileView extends React.Component<FileViewProps, { visibleCommentLines: number[] }> {
-    private renderedRef: HTMLSpanElement;
-
-    constructor(props: FileViewProps) {
-        super(props);
-
-        this.state = {
-            visibleCommentLines: []
-        };
-    }
-
-    public componentDidUpdate(prevProps: FileViewProps, prevState: { visibleCommentLines: number[] }) {
-        if (!PathPairs.equal(prevProps.file.path, this.props.file.path)) {
-            this.setState({ visibleCommentLines: [] });
-        }
-    }
-
-    public componentDidMount() {
-        if (this.props.scrollToFile) {
-            this.props.scrollToFile();
-        }
-
-    }
-
-    private hideLine(line: number) {
-        this.setState({
-            visibleCommentLines: this.state.visibleCommentLines.filter(f => f != line)
-        });
-    }
-
-    private showLine(line: number) {
-        this.setState({
-            visibleCommentLines: [...this.state.visibleCommentLines, line]
-        });
-
-        setTimeout(() => {
-            document.getElementById(getNewDiscussionTextAreaId(line.toString())).focus();
-        }, 0);
-    }
-
-    render(): JSX.Element {
-        const { file, commentActions } = this.props;
-
-        const fileDiscussions = this.props.comments
-            .filter(f => f.fileId == file.fileId);
-
-        const unpublishedDiscussion = this.props.unpublishedFileDiscussions
-            .filter(f => f.fileId  == file.fileId);
-
-        const lineCommentsActions: LineCommentsActions = {
-            hideCommentsForLine: l => this.hideLine(l),
-            showCommentsForLine: l => this.showLine(l),
-            startFileDiscussion: (lineNumber, content, needResolution) => {
-                this.props.startFileDiscussion(file.fileId, lineNumber, content, needResolution)
-            }
-        }
-
-        const diffTypes = {
-            'modified': 'modify' as DiffType,
-            'created': 'add' as DiffType,
-            'deleted': 'delete' as DiffType,
-            'renamed': 'modify' as DiffType
-        };
-
-        const language = file.path.newPath.split('.').slice(-1)[0];
-
-        return (
-            <span ref={span => this.renderedRef = span} key={file.fileId}>
-                <FileSummary file={file} />
-                {file.diff ?
-                    <CommentedDiffView
-                        diffInfo={file.diff}
-                        comments={fileDiscussions.concat(unpublishedDiscussion)}
-                        commentActions={commentActions}
-                        leftSideRevision={file.fileToReview.previous}
-                        rightSideRevision={file.fileToReview.current}
-                        visibleCommentLines={this.state.visibleCommentLines}
-                        lineCommentsActions={lineCommentsActions}
-                        pendingResolved={this.props.pendingResolved}
-                        unpublishedReplies={this.props.unpublishedReplies}
-                        currentUser={this.props.currentUser}
-                        contents={this.props.file.diff.contents.review}
-                        type={diffTypes[this.props.file.fileToReview.changeType]}
-                        language={language}
-                    />
-                    : null}
-            </span>
-        );
-    }
-}
-
-const NoFileView = () => {
-    return (
-        <span className="no-file-selected">Select file to see diff</span>
-    )
-};
+import { NoFileView } from "./sections/fileView";
+import FileList from '@src/fileList';
+import DiffHeader from "./sections/DiffHeader";
+import DiffContent from './sections/diffContent';
 
 export type SelectFileForViewHandler = (fileId: FileId) => void;
 export type OnShowFileHandlerAvailable = (handler: () => void) => void;
@@ -151,16 +31,8 @@ export interface Props {
     reviewedFiles: FileId[];
     publishReview(): void;
     onShowFileHandlerAvailable: OnShowFileHandlerAvailable;
-    reviewId: ReviewId;
     fileComments: FileDiscussion[];
-    unpublishedFileDiscussion: FileDiscussion[];
-    startFileDiscussion(fileId: FileId, lineNumber: number, content: string, needsResolution: boolean): void;
-    commentActions: DiscussionActions;
-    pendingResolved: string[];
-    unpublishedReplies: CommentReply[];
-    currentUser: UserState;
     markNonEmptyAsViewed: any;
-    vsCodeWorkspace: string;
 }
 
 export default class RangeInfo extends React.Component<Props, { stickyContainer: HTMLDivElement }> {
@@ -186,88 +58,23 @@ export default class RangeInfo extends React.Component<Props, { stickyContainer:
         }
     }
 
-    private _findNextFile = (current: FileId, direction: 1 | -1, predicate: (FileToReview) => boolean): FileToReview => {
-        const changes = this.props.filesToReview;
-        const currentIndex = changes.findIndex(p => p.fileId ==this.props.selectedFile.fileId);
-
-        if (currentIndex == -1) {
-            return null;
-        }
-
-        let index = currentIndex;
-
-        while (true) {
-            index += direction;
-
-            if (index == -1) {
-                index = changes.length - 1;
-            } else if (index == changes.length) {
-                index = 0;
-            }
-
-            if (index == currentIndex) {
-                return changes[currentIndex];
-            }
-
-            const candidate = changes[index];
-
-            if (predicate(candidate)) {
-                return candidate;
-            }
-        }
-    }
-
-    private _findNextUnreviewedFile = (current: FileId, direction: 1 | -1): FileToReview => {
-        const predicate = (file: FileToReview) => {
-            const filesReviewedByUser: FileId[] = this.props.reviewedFiles;
-
-            const fileDiscussions = this.props.fileComments
-                .filter(f => f.fileId == file.fileId)
-                .filter(f => f.state === "NeedsResolution");
-
-            if (fileDiscussions.length > 0) {
-                return true;
-            }
-
-            if (filesReviewedByUser.indexOf(file.fileId) == -1) {
-                return true;
-            }
-
-            return false;
-        }
-        
-        return this._findNextFile(current, direction, predicate);
-    }
-
-    private _findNextFileWithUnresolvedComment = (current: FileId, direction: 1 | -1): FileToReview => {
-        const predicate = (file: FileToReview) => {
-            const fileDiscussions = this.props.fileComments
-                .filter(f => f.fileId == file.fileId)
-                .filter(f => f.state === "NeedsResolution" 
-                        && f.comment.children.length == 0);
-
-            if (fileDiscussions.length > 0) {
-                return true;
-            }
-
-            return false;
-        }
-
-        return this._findNextFile(current, direction, predicate);
-    }
-
     render() {
         const { selectedFile, onSelectFileForView } = this.props;
 
-        const menuItems = [];
         let reviewHotKeys = {}
 
         if (selectedFile) {
-            const nextFile = this._findNextUnreviewedFile(selectedFile.fileId, 1);
-            const prevFile = this._findNextUnreviewedFile(selectedFile.fileId, -1);
+            const fileList = new FileList(
+                this.props.filesToReview,
+                this.props.selectedFile.fileId,
+                this.props.reviewedFiles,
+                this.props.fileComments
+            );
+            const nextFile = fileList.nextUnreviewedFile(+1);
+            const prevFile = fileList.nextUnreviewedFile(-1);
 
-            const nextFileWithUnresolvedComment = this._findNextFileWithUnresolvedComment(selectedFile.fileId, 1);
-            const prevFileWithUnresolvedComment = this._findNextFileWithUnresolvedComment(selectedFile.fileId, -1);
+            const nextFileWithUnresolvedComment = fileList.nextFileWithUnresolvedComment(+1);
+            const prevFileWithUnresolvedComment = fileList.nextFileWithUnresolvedComment(-1);
 
             reviewHotKeys = {
                 '[': () => prevFile && onSelectFileForView(prevFile.fileId),
@@ -278,86 +85,6 @@ export default class RangeInfo extends React.Component<Props, { stickyContainer:
                 'ctrl+Enter': this.props.publishReview,
                 'ctrl+y': () => this.props.markNonEmptyAsViewed()
             };
-
-            menuItems.push(
-                <ReviewMode key="review-mark">
-                    <ReviewMode.Reviewer>
-                        <Menu.Item fitted>
-                            <Popup
-                                trigger={<ReviewMark reviewed={this.props.selectedFile.isReviewed} onClick={this._changeFileReviewState} />}
-                                content="Toggle review status"
-                            />
-                        </Menu.Item>
-                    </ReviewMode.Reviewer>
-                    <ReviewMode.Author>
-                        <Menu.Item fitted>
-                            <Icon circular inverted name='eye' color='grey' />
-                        </Menu.Item>
-                    </ReviewMode.Author>
-                </ReviewMode>);
-            menuItems.push(<Menu.Item fitted key="refresh-diff">
-                <Popup
-                    trigger={<Icon onClick={() => onSelectFileForView(selectedFile.fileId)} name="redo" circular link color="blue"></Icon>}
-                    content="Refresh file diff"
-                />
-            </Menu.Item>);
-            menuItems.push(<Menu.Item fitted key="file-navigation">
-                {prevFile &&
-                    <Popup
-                        trigger={<FileLink reviewId={this.props.reviewId} fileId={prevFile.fileId} >
-                            <Icon name="step backward" circular link /></FileLink>}
-                        content="Previous unreviewed file"
-                    />}
-                {nextFile &&
-                    <Popup
-
-                        trigger={<FileLink reviewId={this.props.reviewId} fileId={nextFile.fileId} >
-                            <Icon name="step forward" circular link /></FileLink>}
-                        content="Next unreviewed file"
-                    />}
-            </Menu.Item>);
-            menuItems.push(<Menu.Item fitted key="file-path">
-                <span className="file-path">{selectedFile.path.newPath}</span>
-            </Menu.Item>);
-
-            const downloadFile = () => {
-                const content = JSON.stringify({
-                    current: this.props.selectedFile.diff.contents.review.current,
-                    previous: this.props.selectedFile.diff.contents.review.previous,
-                });
-                const f = new File([content], 'state.json', {type: "application/octet-stream"});
-                const url = window.URL.createObjectURL(f);
-                console.log(url);
-                const a = document.createElement('a');
-                a.href = url;
-                a.click();
-                window.URL.revokeObjectURL(url);
-            };
-
-            menuItems.push(
-                <Menu.Item fitted key="download-diff">
-                    <Popup
-                        trigger={<Icon onClick={downloadFile} name="download" circular link color="blue"></Icon>}
-                        content="Download"
-                    />
-                </Menu.Item>
-            );
-
-            if ((this.props.vsCodeWorkspace || '').length > 0)
-            {
-                const workspacePath = encodeURI(this.props.vsCodeWorkspace.trim().replace('\\', '/').replace(/\/+$/, ''));
-
-                menuItems.push(
-                    <Menu.Item fitted key="vscode-diff">
-                        <Popup
-                            trigger={
-                                <img src={vscodeLogo} className="vscode-icon" onClick={() => window.open(`vscode://file/${workspacePath}/${selectedFile.path.newPath}`)} />                            
-                            }
-                            content="Open in VS Code"
-                        />
-                    </Menu.Item>
-                );
-            }
         } else if (this.props.filesToReview.length > 0) {
             const firstFile = this.props.filesToReview[0].fileId;
             const lastFile = this.props.filesToReview[this.props.filesToReview.length - 1].fileId;
@@ -369,43 +96,16 @@ export default class RangeInfo extends React.Component<Props, { stickyContainer:
             };
         }
 
-        const selectableFiles = this.props.filesToReview.map(i => ({ id: i.fileId, name: i.reviewFile }));
-
         return (
             <div ref={this._handleRef}>
                 <HotKeys config={reviewHotKeys} />
                 <Segment>
                     <Sticky context={this.state.stickyContainer} id="file-sticky">
-                        <Menu secondary id="file-menu">
-                            {menuItems}
-                            <Menu.Menu position='right'>
-                                <Menu.Item>
-                                    <PublishButton />
-                                    &nbsp;
-                                    <ChangedFileTreePopup
-                                        files={selectableFiles}
-                                        selected={selectedFile ? selectedFile.fileId : null}
-                                        reviewedFiles={this.props.reviewedFiles}
-                                        onSelect={onSelectFileForView}
-                                        reviewId={this.props.reviewId}
-                                    />
-                                </Menu.Item>
-                            </Menu.Menu>
-                        </Menu>
+                        <DiffHeader onSelectFileForView={this.props.onSelectFileForView} />
                     </Sticky>
                     <div>
                         {selectedFile ?
-                            <FileView
-                                file={selectedFile}
-                                commentActions={this.props.commentActions}
-                                comments={this.props.selectedFile.discussions}
-                                startFileDiscussion={this.props.startFileDiscussion}
-                                unpublishedFileDiscussions={this.props.unpublishedFileDiscussion}
-                                pendingResolved={this.props.pendingResolved}
-                                unpublishedReplies={this.props.unpublishedReplies}
-                                currentUser={this.props.currentUser}
-                                scrollToFile={this.scrollToFile}
-                            />
+                            <DiffContent scrollToFile={this.scrollToFile} />
                             : <NoFileView />
                         }
                     </div>
--- CodeSaw.Web/frontend/pages/review/sections/file.tsx
+++ CodeSaw.Web/frontend/pages/review/sections/file.tsx
@@ -2,10 +2,9 @@ import { connect } from "react-redux";
 import * as React from "react";
 import RangeInfo, { ReviewFileActions, SelectFileForViewHandler, OnShowFileHandlerAvailable } from "../rangeInfo";
 import { Dispatch } from "redux";
-import { RootState, UserState } from "@src/rootState";
-import { ReviewInfo, FileId, CommentReply, FileDiscussion, ReviewId } from "@api/reviewer";
-import { markEmptyFilesAsReviewed, reviewFile, unreviewFile, publishReview, replyToComment, editUnpublishedComment, removeUnpublishedComment, resolveDiscussion, unresolveDiscussion, startReviewDiscussion, startFileDiscussion, FileInfo, selectFileForView } from "../state";
-import { DiscussionActions } from "../commentsView";
+import { RootState } from "@src/rootState";
+import { ReviewInfo, FileId, ReviewId } from "@api/reviewer";
+import { markEmptyFilesAsReviewed, reviewFile, unreviewFile, publishReview, FileInfo, selectFileForView } from "../state";
 import { createLinkToFile } from "../FileLink";
 import { History } from "history";
 
@@ -20,18 +19,11 @@ interface OwnProps {
 interface StateProps {
     currentReview: ReviewInfo;
     reviewedFiles: FileId[];
-    currentUser: UserState;
-    vsCodeWorkspace: string;
-    unpublishedReplies: CommentReply[];
-    unpublishedFileDiscussion: FileDiscussion[];
-    unpublishedResolvedDiscussions: string[];
     selectedFile: FileInfo;
 }
 
 interface DispatchProps {
     markNonEmptyAsViewed(): void;
-    discussionActions: DiscussionActions;
-    startFileDiscussion(fileId: FileId, lineNumber: number, content: string, needsResolution: boolean, currentUser?: UserState): void;
     publishReview(): void;
     reviewFile: ReviewFileActions;
     selectFileForView: SelectFileForViewHandler;
@@ -67,56 +59,24 @@ const File = (props: Props): JSX.Element => {
         reviewedFiles={props.reviewedFiles}
         publishReview={props.publishReview}
         onShowFileHandlerAvailable={props.onShowFileHandlerAvailable}
-        reviewId={props.reviewId}
         fileComments={props.currentReview.fileDiscussions}
-        startFileDiscussion={props.startFileDiscussion}
-        unpublishedFileDiscussion={props.unpublishedFileDiscussion}
-        commentActions={props.discussionActions}
-        pendingResolved={props.unpublishedResolvedDiscussions}
-        unpublishedReplies={props.unpublishedReplies}
-        currentUser={props.currentUser}
         markNonEmptyAsViewed={props.markNonEmptyAsViewed}
-        vsCodeWorkspace={props.vsCodeWorkspace}
     />
 };
 
 export default connect(
     (state: RootState): StateProps => ({
-        currentUser: state.currentUser,
         currentReview: state.review.currentReview,
         reviewedFiles: state.review.reviewedFiles,
-        unpublishedFileDiscussion: state.review.unpublishedFileDiscussions,
-        unpublishedResolvedDiscussions: state.review.unpublishedResolvedDiscussions,
-        unpublishedReplies: state.review.unpublishedReplies,
-        vsCodeWorkspace: state.review.vsCodeWorkspace,
         selectedFile: state.review.selectedFile,
     }),
     (dispatch: Dispatch, ownProps: OwnProps) => ({
         selectFileForView: (fileId) => dispatch(selectFileForView({ fileId })),
         markNonEmptyAsViewed: () => dispatch(markEmptyFilesAsReviewed({})),
-        startFileDiscussion: (fileId, lineNumber, content, needsResolution, currentUser) => dispatch(startFileDiscussion({ fileId, lineNumber, content, needsResolution, currentUser })),
         reviewFile: {
             review: (path) => dispatch(reviewFile({ path })),
             unreview: (path) => dispatch(unreviewFile({ path })),
         },
         publishReview: () => dispatch(publishReview({ fileToLoad: ownProps.fileId })),
-        addNew: (content, needsResolution, currentUser) => dispatch(startReviewDiscussion({ content, needsResolution, currentUser })),
-        discussionActions: {
-            addReply: (parentId, content) => dispatch(replyToComment({ parentId, content })),
-            editReply: (commentId, content) => dispatch(editUnpublishedComment({ commentId, content })),
-            removeUnpublishedComment: (commentId) => dispatch(removeUnpublishedComment({ commentId })),
-            resolve: (discussionId) => dispatch(resolveDiscussion({ discussionId })),
-            unresolve: (discussionId) => dispatch(unresolveDiscussion({ discussionId })),
-        }
-    }),
-    (stateProps: StateProps, dispatchProps, ownProps: OwnProps): Props => ({
-        ...stateProps,
-        ...dispatchProps,
-        ...ownProps,
-        discussionActions: {
-            ...dispatchProps.discussionActions,
-            addNew: (content, needsResolution) => dispatchProps.addNew(content, needsResolution, stateProps.currentUser),
-        },
-        startFileDiscussion: (path, lineNumber, content, needsResolution) => dispatchProps.startFileDiscussion(path, lineNumber, content, needsResolution, stateProps.currentUser),
     })
 )(File);
