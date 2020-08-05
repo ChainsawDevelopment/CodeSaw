b44db0c10833ea9d1f5e6e966d497609b51dcacc
Maciej Nowak <maciejt.nowak@gmail.com>
2020-08-05 13:01:15 +0200

    Feature toggle component

---
+++ CodeSaw.Tests/FeatureToggleTest.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using CodeSaw.Web;
+using Microsoft.FSharp.Core;
+using NUnit.Framework;
+
+namespace CodeSaw.Tests
+{
+    public class FeatureToggleTest
+    {
+        [Test]
+        public void CheckIfActive_Active()
+        {
+            var toggler = new FeatureToggle();
+            toggler.EnableFeatures(new List<string> {"Feature1"});
+            var feature = toggler.For("Feature1");
+
+            Assert.That(feature.IsActive, Is.True);
+        }
+
+        [Test]
+        public void CheckIfActive_NotActive()
+        {
+            var toggler = new FeatureToggle();
+            var feature = toggler.For("Feature1");
+
+            Assert.That(feature.IsActive, Is.False);
+        }
+
+        [Test]
+        public void DoIfActive_Active()
+        {
+            var toggler = new FeatureToggle();
+            toggler.EnableFeatures(new List<string> { "Feature1" });
+            var feature = toggler.For("Feature1");
+
+            bool flag = false;
+
+            feature.WhenActive(() => flag = true);
+
+            Assert.That(flag, Is.True);
+        }
+
+        [Test]
+        public void DoIfActive_NotActive()
+        {
+            var toggler = new FeatureToggle();
+            var feature = toggler.For("Feature1");
+
+            bool flag = false;
+
+            feature.WhenActive(() => flag = true);
+
+            Assert.That(flag, Is.False);
+        }
+
+        [Test]
+        public void DoIfInactive_Active()
+        {
+            var toggler = new FeatureToggle();
+            toggler.EnableFeatures(new List<string> { "Feature1" });
+            var feature = toggler.For("Feature1");
+
+            bool flag = false;
+
+            feature.WhenInactive(() => flag = true);
+
+            Assert.That(flag, Is.False);
+        }
+
+        [Test]
+        public void DoIfInactive_NotActive()
+        {
+            var toggler = new FeatureToggle();
+            var feature = toggler.For("Feature1");
+
+            bool flag = false;
+
+            feature.WhenInactive(() => flag = true);
+
+            Assert.That(flag, Is.True);
+        }
+
+        [Test]
+        public void Do_Active()
+        {
+            var toggler = new FeatureToggle();
+            toggler.EnableFeatures(new List<string> { "Feature1" });
+            var feature = toggler.For("Feature1");
+            int i = 0;
+
+            feature.Do(active: () => i = 1, inactive: () => i = 2);
+
+            Assert.That(i, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Do_Inactive()
+        {
+            var toggler = new FeatureToggle();
+            var feature = toggler.For("Feature1");
+            int i = 0;
+
+            feature.Do(active: () => i = 1, inactive: () => i = 2);
+
+            Assert.That(i, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Map_Active()
+        {
+            var toggler = new FeatureToggle();
+            toggler.EnableFeatures(new List<string> { "Feature1" });
+            var feature = toggler.For("Feature1");
+
+            int i = feature.Map(active: () => 1, inactive: () => 2);
+
+            Assert.That(i, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Map_Inactive()
+        {
+            var toggler = new FeatureToggle();
+            var feature = toggler.For("Feature1");
+
+            int i = feature.Map(active: () => 1, inactive: () => 2);
+
+            Assert.That(i, Is.EqualTo(2));
+        }
+    }
+}
---
+++ CodeSaw.Web/FeatureToggle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeSaw.Web
+{
+    public class FeatureToggle
+    {
+        private readonly HashSet<string> _enabledFeatures;
+
+        public FeatureToggle()
+        {
+            _enabledFeatures = new HashSet<string>();
+        }
+
+        public Actions For(string featureName)
+        {
+            return new Actions(featureName, _enabledFeatures.Contains(featureName));
+        }
+
+        public void EnableFeatures(IEnumerable<string> featureNames)
+        {
+            _enabledFeatures.UnionWith(featureNames);
+        }
+
+        public class Actions
+        {
+            public string FeatureName { get; }
+
+            public bool IsActive { get; }
+
+            public Actions(string featureName, bool active)
+            {
+                FeatureName = featureName;
+                IsActive = active;
+            }
+
+            public void WhenActive(Action action)
+            {
+                if (IsActive) action();
+            }
+
+            public void WhenInactive(Action action)
+            {
+                if (!IsActive) action();
+            }
+
+            public void Do(Action active, Action inactive)
+            {
+                if (IsActive)
+                    active();
+                else
+                    inactive();
+            }
+
+            public TResult Map<TResult>(Func<TResult> active, Func<TResult> inactive)
+            {
+                return IsActive ? active() : inactive();
+            }
+        }
+    }
+}
