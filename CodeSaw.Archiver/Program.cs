124bbe1299272f24b5a3f20367ffb89a409e8360
Marcin Drobik <mdrobik@kplabs.pl>
2020-11-29 14:09:54 +0100

    CodeSaw.Archiver - app to archiver old revisions and remove their tags

---
+++ CodeSaw.Archiver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeSaw.Archiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
