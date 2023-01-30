888608393f864b3452e2e81a81e35c77a968a022
Maciej Nowak <maciejt.nowak@gmail.com>
2023-01-30 19:08:40 +0100

    Add link to merge request on MR edit event

--- CodeSaw.Web/BackgroundActions/AddLinkToReview.cs
+++ CodeSaw.Web/BackgroundActions/AddLinkToReview.cs
@@ -1,21 +1,36 @@
 ﻿using System.Threading.Tasks;
+using CodeSaw.RepositoryApi;
 using CodeSaw.Web.Cqrs;
 using CodeSaw.Web.Modules.Api.Commands;
 using CodeSaw.Web.Modules.Hooks;
 
 namespace CodeSaw.Web.BackgroundActions
 {
-    public class AddLinkToReview : IHandle<NewReview>
+    public class AddLinkToReview : IHandle<NewReview>, IHandle<ReviewChangedExternallyEvent>
     {
         private readonly ICommandDispatcher _commands;
+        private readonly IRepository _repository;
 
-        public AddLinkToReview(ICommandDispatcher commands)
+        public AddLinkToReview(ICommandDispatcher commands, IRepository repository)
         {
             _commands = commands;
+            _repository = repository;
         }
 
         public async Task Handle(NewReview @event)
         {
+            await _commands.Execute(new RegisterReviewLink(@event.ReviewId.ProjectId, @event.ReviewId.ReviewId));
+        }
+
+        public async Task Handle(ReviewChangedExternallyEvent @event)
+        {
+            var mr = await _repository.GetMergeRequestInfo(@event.ReviewId.ProjectId, @event.ReviewId.ReviewId);
+            if(mr.Description.Contains("There's a better review for that:"))
+            {
+                NLog.LogManager.GetLogger("AAA").Info("Link present");
+                return;
+            }
+
             await _commands.Execute(new RegisterReviewLink(@event.ReviewId.ProjectId, @event.ReviewId.ReviewId));
         }
     }
